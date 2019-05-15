732bb03c53abdbe34578b5839ee5372a6a7e4912
Elvin <elveng@gmail.com>
2019-05-14 21:56:00 -0400

    testing route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,10 +3,11 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-
+/*
 router.get("/", function(req, res) {
     res.redirect("/burgers");
 });
+*/
 
 router.get("/burgers", function(req, res) {
     var host = req.get('Host');
